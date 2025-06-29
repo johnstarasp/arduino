a371eafd2cc00e96979ff3858e29233b670653e9
yachtcharterdev <yachtchartergreecesales@gmail.com>
2025-06-30 01:51:18 +0300

    te

--- magentTest3.py
+++ magentTest3.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as GPIO
+import time
 
 def pulse_detected(channel):
     print("Pulse detected!")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(17, GPIO.FALLING, callback=pulse_detected, bouncetime=10)
 
-print("OK. Waiting...")
 try:
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=pulse_detected, bouncetime=10)
+    print("Waiting for pulses. Ctrl+C to stop.")
     while True:
-        pass
+        time.sleep(1)
 except KeyboardInterrupt:
+    print("Stopped.")
+finally:
     GPIO.cleanup()
