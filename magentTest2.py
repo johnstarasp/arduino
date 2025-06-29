68c248b7274b38029cde33e2ef2cf367c27bdc0c
yachtcharterdev <yachtchartergreecesales@gmail.com>
2025-06-30 01:46:56 +0300

    test

--- magentTest2.py
+++ magentTest2.py
@@ -1,7 +1,12 @@
 import RPi.GPIO as GPIO
 
 HALL_SENSOR_PIN = 17
+pulse_count = 0
 
+
+def pulse_detected(channel):
+    global pulse_count
+    pulse_count += 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(HALL_SENSOR_PIN, GPIO.FALLING, callback=pulse_detected, bouncetime=10)
