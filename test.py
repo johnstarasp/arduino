17cfea86b4b836f9d365a64443457f863c525372
yachtcharterdev <yachtchartergreecesales@gmail.com>
2025-06-29 19:16:08 +0300

    test

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ def send_sms():
         at('AT+CREG?')
         at('AT+CSCA?')         # Check SMS center number
         at('AT+CMGF=1')        # Set SMS text mode
+        at("AT+CSCA='+306971000000'")
         at(f'AT+CMGS="{PHONE}"', wait=2)
         ser.write((MESSAGE + '\x1A').encode())
         time.sleep(5)
