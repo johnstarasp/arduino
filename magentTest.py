e34c3b483047071329d6ce3115a499eac00b4b4b
John Staras <staras.ioannis@nbg.gr>
2025-08-16 22:31:19 +0300

    delete everything

--- magentTest.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-try:
-    print("Pin 17 status:", GPIO.input(17))
-finally:
-    GPIO.cleanup()
